19cc650d176537cbf5b37a1669fc65c49b27e593
liudyboy <787896130@qq.com>
2019-05-21 15:03:29 +0800

    $date

---
+++ device_cloud.py
@@ -0,0 +1,314 @@
+from __future__ import print_function
+import logging
+import numpy as np
+
+import grpc
+import communication_pb2
+import communication_pb2_grpc
+import pickle
+import chainer
+from chainer import backend
+from chainer import backends
+from chainer.backends import cuda
+from chainer import Function, gradient_check, report, training, utils, Variable
+from chainer import datasets, iterators, optimizers, serializers
+from chainer import Link, Chain, ChainList
+import chainer.functions as F
+import chainer.links as L
+from chainer.training import extensions
+from chainer import link
+from chainer import initializers
+from chainer import utils
+from chainer import variable
+import update
+import layersM as layers
+import tiny_utils as utils
+import gc
+import time
+import sys
+import args
+import _thread
+import connect
+
+def init_layers(process_layers):
+    global conv1, conv2, conv3, conv4, conv5, fc6, fc7, fc8, max_pool1, max_pool2, max_pool5
+    conv_stride = [4, 4]
+    conv_stride = [4, 4]
+    if 1 in process_layers:
+        conv1 = layers.conv2d(filters=96, kernel=[11, 11], padding='SAME', name='conv1', activation='relu', normalization='local_response_normalization', stride=conv_stride )
+        c = np.load("init_wb/conv1.npz")
+        conv1.w, conv1.b = c['w'], c['b']
+        conv1.w, conv1.b = chainer.as_variable(conv1.w), chainer.as_variable(conv1.b)
+    if 2 in process_layers:
+        max_pool1 = layers.max_pool2d(ksize=[3, 3], stride=[2, 2])
+    if 3 in process_layers:
+        conv2 = layers.conv2d(filters=256, kernel=[5, 5], padding='SAME', name='conv2', activation='relu', normalization="local_response_normalization", stride=[1, 1] )
+        c = np.load("init_wb/conv2.npz")
+        conv2.w, conv2.b = c['w'], c['b']
+        conv2.w, conv2.b = chainer.as_variable(conv2.w), chainer.as_variable(conv2.b)
+    if 4 in process_layers:
+        max_pool2 = layers.max_pool2d(ksize=[3, 3], stride=[2, 2])
+    if 5 in process_layers:
+        conv3 = layers.conv2d(filters=384, kernel=[3, 3], padding='SAME', name='conv3', activation='relu', stride=[1, 1] )
+        c = np.load("init_wb/conv3.npz")
+        conv3.w, conv3.b = c['w'], c['b']
+        conv3.w, conv3.b = chainer.as_variable(conv3.w), chainer.as_variable(conv3.b)
+    if 6 in process_layers:
+        conv4 = layers.conv2d(filters=384, kernel=[3, 3], padding='SAME', name='conv4', activation='relu', stride=[1, 1] )
+        c = np.load("init_wb/conv4.npz")
+        conv4.w, conv4.b = c['w'], c['b']
+        conv4.w, conv4.b = chainer.as_variable(conv4.w), chainer.as_variable(conv4.b)
+    if 7 in process_layers:
+        conv5 = layers.conv2d(filters=256, kernel=[3, 3], padding='SAME', name='conv5', activation='relu', stride=[1, 1] )
+        c = np.load("init_wb/conv5.npz")
+        conv5.w, conv5.b = c['w'], c['b']
+        conv5.w, conv5.b = chainer.as_variable(conv5.w), chainer.as_variable(conv5.b)
+    if 8 in process_layers:
+        max_pool5 = layers.max_pool2d(ksize=[3, 3], stride=[2, 2])
+    if 9 in process_layers:
+        fc6 = layers.dense(4096, activation='relu', dropout=True, name='fc6' )
+        c = np.load("init_wb/fc6.npz")
+        fc6.w, fc6.b = c['w'], c['b']
+        fc6.w, fc6.b = chainer.as_variable(fc6.w), chainer.as_variable(fc6.b)
+    if 10 in process_layers:
+        fc7 = layers.dense(4096, activation='relu', dropout=True, name='fc7' )
+        c = np.load("init_wb/fc7.npz")
+        fc7.w, fc7.b = c['w'], c['b']
+        fc7.w, fc7.b = chainer.as_variable(fc7.w), chainer.as_variable(fc7.b)
+    if 11 in process_layers:
+        fc8 = layers.dense(200, activation='relu', name='fc8' )
+        c = np.load("init_wb/fc8.npz")
+        fc8.w, fc8.b = c['w'], c['b']
+        fc8.w, fc8.b = chainer.as_variable(fc8.w), chainer.as_variable(fc8.b)
+
+
+def forward(out, process_layers):
+    if 1 in process_layers:
+        out = conv1.forward(out)
+    if 2 in process_layers:
+        out = max_pool1.forward(out)
+    if 3 in process_layers:
+        out = conv2.forward(out)
+    if 4 in process_layers:
+        out = max_pool2.forward(out)
+    if 5 in process_layers:
+        out = conv3.forward(out)
+    if 6 in process_layers:
+        out = conv4.forward(out)
+    if 7 in process_layers:
+        out = conv5.forward(out)
+    if 8 in process_layers:
+        out = max_pool5.forward(out)
+    if 9 in process_layers:
+        out = fc6.forward(out)
+    if 10 in process_layers:
+        out = fc7.forward(out)
+    if 11 in process_layers:
+        out = fc8.forward(out)
+    return out
+
+def cal_gradients(d_out, process_layers, Y=None):
+    if 11 in process_layers:
+        loss = F.softmax_cross_entropy(d_out, Y)
+        accuracy = F.accuracy(d_out, Y)
+        print('loss: {}'.format(loss))
+        print('accuracy: {}'.format(accuracy))
+        d_out = chainer.grad([loss], [d_out])
+        if isinstance(d_out, (list)):
+            d_out = d_out[0]
+        d_out = fc8.backward(d_out)
+    if 10 in process_layers:
+        d_out = fc7.backward(d_out)
+    if 9 in process_layers:
+        d_out = fc6.backward(d_out)
+    if 8 in process_layers:
+        d_out = max_pool5.backward(d_out)
+    if 7 in process_layers:
+        d_out = conv5.backward(d_out)
+    if 6 in process_layers:
+        d_out = conv4.backward(d_out)
+    if 5 in process_layers:
+        d_out = conv3.backward(d_out)
+    if 4 in process_layers:
+        d_out = max_pool2.backward(d_out)
+    if 3 in process_layers:
+        d_out = conv2.backward(d_out)
+    if 2 in process_layers:
+        d_out = max_pool1.backward(d_out)
+    if 1 in process_layers:
+        d_out = conv1.backward(d_out)
+
+    del d_out
+
+def update_one_layer_parameters(layer_num, batch_size):
+    if 1 == layer_num:
+        conv1.update_parameters(batch=batch_size)
+    if 2 == layer_num:
+        pass          # it is maxpool layer needn't exchange gradients
+    if 3 == layer_num:
+        conv2.update_parameters(batch=batch_size)
+    if 4 == layer_num:
+        pass          # it is maxpool layer needn't exchange gradients
+    if 5 == layer_num:
+        conv3.update_parameters(batch=batch_size)
+    if 6 == layer_num:
+        conv4.update_parameters(batch=batch_size)
+    if 7 == layer_num:
+        conv5.update_parameters(batch=batch_size)
+    if 8 == layer_num:
+        pass          # it is maxpool layer needn't exchange gradients
+    if 9 == layer_num:
+        fc6.update_parameters(batch=batch_size)
+    if 10 == layer_num:
+        fc7.update_parameters(batch=batch_size)
+    if 11 == layer_num:
+        fc8.update_parameters(batch=batch_size)
+    return
+
+def process_gradients_exchange(layer_num):
+    global edge_address
+    destination = edge_address
+    if 1 == layer_num:
+        grads_w, grads_bias = conv1.get_params_grad()
+        grads_w, grads_bias = get_one_layer_gradients(destination, grads_w, grads_bias, layer_num)
+        conv1.accumulate_params_grad(grads_w, grads_bias)
+    if 2 == layer_num:
+        pass          # it is maxpool layer needn't exchange gradients
+    if 3 == layer_num:
+        grads_w, grads_bias = conv2.get_params_grad()
+        grads_w, grads_bias = get_one_layer_gradients(destination, grads_w, grads_bias, layer_num)
+        conv2.accumulate_params_grad(grads_w, grads_bias)
+    if 4 == layer_num:
+        pass          # it is maxpool layer needn't exchange gradients
+    if 5 == layer_num:
+        grads_w, grads_bias = conv3.get_params_grad()
+        grads_w, grads_bias = get_one_layer_gradients(destination, grads_w, grads_bias, layer_num)
+        conv3.accumulate_params_grad(grads_w, grads_bias)
+    if 6 == layer_num:
+        grads_w, grads_bias = conv4.get_params_grad()
+        grads_w, grads_bias = get_one_layer_gradients(destination, grads_w, grads_bias, layer_num)
+        conv4.accumulate_params_grad(grads_w, grads_bias)
+    if 7 == layer_num:
+        grads_w, grads_bias = conv5.get_params_grad()
+        grads_w, grads_bias = get_one_layer_gradients(destination, grads_w, grads_bias, layer_num)
+        conv5.accumulate_params_grad(grads_w, grads_bias)
+    if 8 == layer_num:
+        pass          # it is maxpool layer needn't exchange gradients
+    if 9 == layer_num:
+        grads_w, grads_bias = fc6.get_params_grad()
+        grads_w, grads_bias = get_one_layer_gradients(destination, grads_w, grads_bias, layer_num)
+        fc6.accumulate_params_grad(grads_w, grads_bias)
+    if 10 == layer_num:
+        grads_w, grads_bias = fc7.get_params_grad()
+        grads_w, grads_bias = get_one_layer_gradients(destination, grads_w, grads_bias, layer_num)
+        fc7.accumulate_params_grad(grads_w, grads_bias)
+    if 11 == layer_num:
+        grads_w, grads_bias = fc8.get_params_grad()
+        grads_w, grads_bias = get_one_layer_gradients(destination, grads_w, grads_bias, layer_num)
+        fc8.accumulate_params_grad(grads_w, grads_bias)
+
+def init_signal():
+    global finish_flag
+    finish_flag = 0                      # if fnish_flag equal 2 means one epoch training end
+
+def send_raw_data(threadName, threadID, destination, X, Y):
+    global finish_flag
+    print(threadName, " start")
+    connect.conn_send_raw_data(destination, X.array, Y.array)
+    print(threadName, " end")
+    finish_flag += 1
+
+def send_output_data(destination, output, Y):
+    print('Send output to edge')
+    reply = connect.conn_send_device_output_data(destination, output.array, Y.array)
+    print('receive output gradients')
+    return chainer.as_variable(reply)
+
+def get_one_layer_gradients(destination, grads_w, grads_bias, layer_num):
+    # print('get layer ', layer_num, ' gradients')
+    # while True:
+    #     recv_grads_w, recv_grads_bias = connect.conn_get_gradients(destination, grads_w, grads_bias, layer_num)
+    #     if recv_grads_w.shape[0] != 1:
+    #         break;
+    # print('complete get layer', layer_num, ' gradients')
+    # print('layer gradients: ', recv_grads_w.shape)
+    grads_w = chainer.as_variable(grads_w)
+    grads_bias = chainer.as_variable(grads_bias)
+    recv_grads_w, recv_grads_bias = connect.conn_get_gradients(destination, grads_w, grads_bias, layer_num, 'device')
+    return chainer.as_variable(recv_grads_w), chainer.as_variable(recv_grads_bias)
+
+# just in case to wait two new threads complete
+def wait_threads_complete(cloud):
+    target = 1
+    if cloud != 0:
+        target += 1
+    print('wait threads complete')
+    while finish_flag is not target:
+        pass
+    print('one epoch training completed')
+
+
+# edge_address = "192.168.1.77:50055"
+# cloud_address = "192.168.1.70:50052"
+
+cloud_address = "192.168.1.77:50052"
+edge_address = "192.168.1.70:50055"
+
+if __name__ == "__main__":
+    my_args = args.init_args()
+    device_run_layers, cloud_run_layers = my_args.M1, my_args.M2
+    init_layers(np.arange(device_run_layers+1))
+    edge_batch, cloud_batch = 126, 1
+    logging.basicConfig()
+    generations = 10
+    total_batch_size = 128
+    for i in range(generations):
+        init_signal()
+        trainX, trainY = utils.get_batch_data(total_batch_size)
+        trainX = chainer.as_variable(trainX)
+        trainY = chainer.as_variable(trainY.astype(np.int32))
+
+        ts1 = time.time()
+
+        #spilt raw data for edge , cloud
+        batch_size = trainX.shape[0]
+        cloud_X = trainX[batch_size-cloud_batch:]
+        cloud_Y = trainY[batch_size-cloud_batch:]
+        trainX = trainX[:batch_size-cloud_batch]
+        trainY = trainY[:batch_size-cloud_batch]
+        batch_size = trainX.shape[0]
+        edge_X = trainX[batch_size-edge_batch:]
+        edge_Y = trainY[batch_size-edge_batch:]
+        trainX = trainX[:batch_size-edge_batch]
+        trainY = trainY[:batch_size-edge_batch]
+
+        try:
+            if cloud_run_layers != 0:
+                _thread.start_new_thread(send_raw_data, ("send cloud thread", 0, cloud_address, cloud_X, cloud_Y))
+            _thread.start_new_thread(send_raw_data, ("send edge thread", 1, edge_address, edge_X, edge_Y))
+        except:
+            print('send raw thread error')
+        if device_run_layers != 0:
+            process_layers = np.arange(1, device_run_layers+1)
+            output = forward(trainX, process_layers)
+
+            tts2 = time.time()
+            print('forward cost time: ', (tts2 - ts1) * 1000.)
+            output_reply = send_output_data(edge_address, output, trainY)
+
+            tts3 = time.time()
+            cal_gradients(output_reply, process_layers, trainY)
+
+            tts4 = time.time()
+            print('cal gradients cost time: ', (tts4 - tts3) * 1000.)
+
+            for j in process_layers:
+                process_gradients_exchange(j)
+                update_one_layer_parameters(j, total_batch_size)
+
+        wait_threads_complete(cloud_run_layers)
+
+        ts2 = time.time()
+
+        one_epoch_time = (ts2 - ts1) * 1000.
+        print('one epoch training time: ', one_epoch_time)
---
+++ edge_middle.py
@@ -0,0 +1,334 @@
+from concurrent import futures
+import logging
+import grpc
+
+import communication_pb2_grpc
+import communication_pb2
+import time
+import chainer
+from chainer import backend
+from chainer import backends
+from chainer.backends import cuda
+from chainer import Function, gradient_check, report, training, utils, Variable
+from chainer import datasets, iterators, optimizers, serializers
+from chainer import Link, Chain, ChainList
+import chainer.functions as F
+import chainer.links as L
+from chainer.training import extensions
+from chainer import link
+from chainer import initializers
+from chainer import utils
+from chainer import variable
+import update
+import layersM as layers
+import tiny_utils as utils
+import gc
+import time
+import numpy as np
+import pickle
+import sys
+import args
+import connect
+
+
+_ONE_DAY_IN_SECONDS = 60 * 60 * 24
+
+class Connecter(communication_pb2_grpc.CommServicer):
+
+    init_layers_flag = False
+    def init_variables(self):
+        self.TOTAL_BATCH = 128
+        self.edge_address = '192.168.1.70:50055'
+
+    def init_layers(self, process_layers):
+        self.init_layers_flag = True
+        conv_stride = [4, 4]
+
+        if 1 in process_layers:
+            self.conv1 = layers.conv2d(filters=96, kernel=[11, 11], padding='SAME', name='conv1', activation='relu', normalization='local_response_normalization', stride=conv_stride, use_gpu=True)
+            c = np.load("init_wb/conv1.npz")
+            self.conv1.w, self.conv1.b = c['w'], c['b']
+            self.conv1.w, self.conv1.b = chainer.as_variable(self.conv1.w), chainer.as_variable(self.conv1.b)
+        if 2 in process_layers:
+            self.max_pool1 = layers.max_pool2d(ksize=[3, 3], stride=[2, 2], use_gpu=True)
+        if 3 in process_layers:
+            self.conv2 = layers.conv2d(filters=256, kernel=[5, 5], padding='SAME', name='conv2', activation='relu', normalization="local_response_normalization", stride=[1, 1], use_gpu=True)
+            c = np.load("init_wb/conv2.npz")
+            self.conv2.w, self.conv2.b = c['w'], c['b']
+            self.conv2.w, self.conv2.b = chainer.as_variable(self.conv2.w), chainer.as_variable(self.conv2.b)
+        if 4 in process_layers:
+            self.max_pool2 = layers.max_pool2d(ksize=[3, 3], stride=[2, 2], use_gpu=True)
+        if 5 in process_layers:
+            self.conv3 = layers.conv2d(filters=384, kernel=[3, 3], padding='SAME', name='conv3', activation='relu', stride=[1, 1], use_gpu=True)
+            c = np.load("init_wb/conv3.npz")
+            self.conv3.w, self.conv3.b = c['w'], c['b']
+            self.conv3.w, self.conv3.b = chainer.as_variable(self.conv3.w), chainer.as_variable(self.conv3.b)
+        if 6 in process_layers:
+            self.conv4 = layers.conv2d(filters=384, kernel=[3, 3], padding='SAME', name='conv4', activation='relu', stride=[1, 1], use_gpu=True)
+            c = np.load("init_wb/conv4.npz")
+            self.conv4.w, self.conv4.b = c['w'], c['b']
+            self.conv4.w, self.conv4.b = chainer.as_variable(self.conv4.w), chainer.as_variable(self.conv4.b)
+        if 7 in process_layers:
+            self.conv5 = layers.conv2d(filters=256, kernel=[3, 3], padding='SAME', name='conv5', activation='relu', stride=[1, 1], use_gpu=True)
+            c = np.load("init_wb/conv5.npz")
+            self.conv5.w, self.conv5.b = c['w'], c['b']
+            self.conv5.w, self.conv5.b = chainer.as_variable(self.conv5.w), chainer.as_variable(self.conv5.b)
+        if 8 in process_layers:
+            self.max_pool5 = layers.max_pool2d(ksize=[3, 3], stride=[2, 2], use_gpu=True)
+        if 9 in process_layers:
+            self.fc6 = layers.dense(4096, activation='relu', dropout=True, name='fc6', use_gpu=True)
+            c = np.load("init_wb/fc6.npz")
+            self.fc6.w, self.fc6.b = c['w'], c['b']
+            self.fc6.w, self.fc6.b = chainer.as_variable(self.fc6.w), chainer.as_variable(self.fc6.b)
+        if 10 in process_layers:
+            self.fc7 = layers.dense(4096, activation='relu', dropout=True, name='fc7', use_gpu=True)
+            c = np.load("init_wb/fc7.npz")
+            self.fc7.w, self.fc7.b = c['w'], c['b']
+            self.fc7.w, self.fc7.b = chainer.as_variable(self.fc7.w), chainer.as_variable(self.fc7.b)
+        if 11 in process_layers:
+            self.fc8 = layers.dense(200, activation='relu', name='fc8', use_gpu=True)
+            c = np.load("init_wb/fc8.npz")
+            self.fc8.w, self.fc8.b = c['w'], c['b']
+            self.fc8.w, self.fc8.b = chainer.as_variable(self.fc8.w), chainer.as_variable(self.fc8.b)
+
+        return
+
+
+    def cal_forward(self, out, process_layers):
+        if 1 in process_layers:
+            out = self.conv1.forward(out)
+        if 2 in process_layers:
+            out = self.max_pool1.forward(out)
+        if 3 in process_layers:
+            out = self.conv2.forward(out)
+        if 4 in process_layers:
+            out = self.max_pool2.forward(out)
+        if 5 in process_layers:
+            out = self.conv3.forward(out)
+        if 6 in process_layers:
+            out = self.conv4.forward(out)
+        if 7 in process_layers:
+            out = self.conv5.forward(out)
+        if 8 in process_layers:
+            out = self.max_pool5.forward(out)
+        if 9 in process_layers:
+            out = self.fc6.forward(out)
+        if 10 in process_layers:
+            out = self.fc7.forward(out)
+        if 11 in process_layers:
+            out = self.fc8.forward(out)
+        return out
+
+    def cal_gradients(self, d_out, process_layers, Y=None):
+        if 11 in process_layers:
+            loss = F.softmax_cross_entropy(d_out, Y)
+            accuracy = F.accuracy(d_out, Y)
+            print('loss: {}'.format(loss))
+            print('accuracy: {}'.format(accuracy))
+            d_out = chainer.grad([loss], [d_out])
+            if isinstance(d_out, (list)):
+                d_out = d_out[0]
+            d_out = self.fc8.backward(d_out)
+        if 10 in process_layers:
+            d_out = self.fc7.backward(d_out)
+        if 9 in process_layers:
+            d_out = self.fc6.backward(d_out)
+        if 8 in process_layers:
+            d_out = self.max_pool5.backward(d_out)
+        if 7 in process_layers:
+            d_out = self.conv5.backward(d_out)
+        if 6 in process_layers:
+            d_out = self.conv4.backward(d_out)
+        if 5 in process_layers:
+            d_out = self.conv3.backward(d_out)
+        if 4 in process_layers:
+            d_out = self.max_pool2.backward(d_out)
+        if 3 in process_layers:
+            d_out = self.conv2.backward(d_out)
+        if 2 in process_layers:
+            d_out = self.max_pool1.backward(d_out)
+        if 1 in process_layers:
+            d_out = self.conv1.backward(d_out)
+        return
+
+
+
+    def get_one_layer_gradients(self, destination, grads_w, grads_bias, layer_num):
+        # print('get layer ', layer_num, ' gradients')
+        # while True:
+        #     recv_grads_w, recv_grads_bias = connect.conn_get_gradients(destination, grads_w, grads_bias, layer_num)
+        #     if recv_grads_w.shape[0] != 1:
+        #         break
+        # print('get layer ', layer_num, 'gradients completed!')
+        # print('gradients: ', recv_grads_w.shape)
+        grads_w = chainer.as_variable(grads_w)
+        grads_bias = chainer.as_variable(grads_bias)
+        recv_grads_w, recv_grads_bias = connect.conn_get_gradients(destination, grads_w.array, grads_bias.array, layer_num, 'cloud')
+        return chainer.as_variable(recv_grads_w), chainer.as_variable(recv_grads_bias)
+
+
+    def process_gradients_exchange(self, layer_num):
+        edge_address = self.edge_address
+        destination = edge_address
+        if 1 == layer_num:
+            grads_w, grads_bias = self.conv1.get_params_grad()
+            grads_w.to_cpu()
+            grads_bias.to_cpu()
+            grads_w, grads_bias = self.get_one_layer_gradients(destination, grads_w, grads_bias, layer_num)
+            grads_w.to_gpu(0)
+            grads_bias.to_gpu(0)
+            self.conv1.accumulate_params_grad(grads_w, grads_bias)
+        if 2 == layer_num:
+            pass          # it is maxpool layer needn't exchange gradients
+        if 3 == layer_num:
+            grads_w, grads_bias = self.conv2.get_params_grad()
+            grads_w.to_cpu()
+            grads_bias.to_cpu()
+            grads_w, grads_bias = self.get_one_layer_gradients(destination, grads_w, grads_bias, layer_num)
+            grads_w.to_gpu(0)
+            grads_bias.to_gpu(0)
+            self.conv2.accumulate_params_grad(grads_w, grads_bias)
+        if 4 == layer_num:
+            pass          # it is maxpool layer needn't exchange gradients
+        if 5 == layer_num:
+            grads_w, grads_bias = self.conv3.get_params_grad()
+            grads_w.to_cpu()
+            grads_bias.to_cpu()
+            grads_w, grads_bias = self.get_one_layer_gradients(destination, grads_w, grads_bias, layer_num)
+            grads_w.to_gpu(0)
+            grads_bias.to_gpu(0)
+            self.conv3.accumulate_params_grad(grads_w, grads_bias)
+        if 6 == layer_num:
+            grads_w, grads_bias = self.conv4.get_params_grad()
+            grads_w.to_cpu()
+            grads_bias.to_cpu()
+            grads_w, grads_bias = self.get_one_layer_gradients(destination, grads_w, grads_bias, layer_num)
+            grads_w.to_gpu(0)
+            grads_bias.to_gpu(0)
+            self.conv4.accumulate_params_grad(grads_w, grads_bias)
+        if 7 == layer_num:
+            grads_w, grads_bias = self.conv5.get_params_grad()
+            grads_w.to_cpu()
+            grads_bias.to_cpu()
+            grads_w, grads_bias = self.get_one_layer_gradients(destination, grads_w, grads_bias, layer_num)
+            grads_w.to_gpu(0)
+            grads_bias.to_gpu(0)
+            self.conv5.accumulate_params_grad(grads_w, grads_bias)
+        if 8 == layer_num:
+            pass          # it is maxpool layer needn't exchange gradients
+        if 9 == layer_num:
+            grads_w, grads_bias = self.fc6.get_params_grad()
+            grads_w.to_cpu()
+            grads_bias.to_cpu()
+            grads_w, grads_bias = self.get_one_layer_gradients(destination, grads_w, grads_bias, layer_num)
+            grads_w.to_gpu(0)
+            grads_bias.to_gpu(0)
+            self.fc6.accumulate_params_grad(grads_w, grads_bias)
+        if 10 == layer_num:
+            grads_w, grads_bias = self.fc7.get_params_grad()
+            grads_w.to_cpu()
+            grads_bias.to_cpu()
+            grads_w, grads_bias = self.get_one_layer_gradients(destination, grads_w, grads_bias, layer_num)
+            grads_w.to_gpu(0)
+            grads_bias.to_gpu(0)
+            self.fc7.accumulate_params_grad(grads_w, grads_bias)
+        if 11 == layer_num:
+            grads_w, grads_bias = self.fc8.get_params_grad()
+            grads_w.to_cpu()
+            grads_bias.to_cpu()
+            grads_w, grads_bias = self.get_one_layer_gradients(destination, grads_w, grads_bias, layer_num)
+            grads_w.to_gpu(0)
+            grads_bias.to_gpu(0)
+            self.fc8.accumulate_params_grad(grads_w, grads_bias)
+        return
+
+    def update_one_layer_parameters(self, layer_num, batch_size):
+        if 1 == layer_num:
+            self.conv1.update_parameters(batch=batch_size)
+        if 2 == layer_num:
+            pass          # it is maxpool layer needn't exchange gradients
+        if 3 == layer_num:
+            self.conv2.update_parameters(batch=batch_size)
+        if 4 == layer_num:
+            pass          # it is maxpool layer needn't exchange gradients
+        if 5 == layer_num:
+            self.conv3.update_parameters(batch=batch_size)
+        if 6 == layer_num:
+            self.conv4.update_parameters(batch=batch_size)
+        if 7 == layer_num:
+            self.conv5.update_parameters(batch=batch_size)
+        if 8 == layer_num:
+            pass          # it is maxpool layer needn't exchange gradients
+        if 9 == layer_num:
+            self.fc6.update_parameters(batch=batch_size)
+        if 10 == layer_num:
+            self.fc7.update_parameters(batch=batch_size)
+        if 11 == layer_num:
+            self.fc8.update_parameters(batch=batch_size)
+        return
+
+
+    def send_output_data(self, destination, output, Y):
+        print('Send output to edge')
+        output.to_cpu()
+        reply = connect.conn_send_cloud_output_data(destination, output.array, Y.array)
+        reply = chainer.as_variable(reply)
+        reply.to_gpu(0)
+        return reply
+
+    # Function call by device
+    # Params: raw_data_x, layer_y
+    def process_raw_data(self, request, context):
+        if self.init_layers_flag is False:
+            self.Log('Initial Model layers')
+            my_args = args.args_prase()
+            self.device_run_layers, self.cloud_run_layers = my_args.M1, my_args.M2
+            process_layers = np.arange(self.cloud_run_layers+1)
+            self.init_layers(process_layers)
+        self.init_variables()
+
+
+        self.raw_input = chainer.as_variable(pickle.loads(request.raw_x))
+        self.Y = chainer.as_variable(pickle.loads(request.Y))
+        process_layers = np.arange(1, self.cloud_run_layers+1)
+        output = self.cal_forward(self.raw_input, process_layers)
+
+        # wait edge ready for start a new epoch
+        while connect.conn_get_singal_for_new_epoch(self.edge_address) is False:
+            pass
+
+
+
+        output_reply = self.send_output_data(self.edge_address, output, self.Y)
+
+
+
+        self.cal_gradients(output_reply, process_layers, self.Y)
+
+        for j in process_layers:
+            ts1 = time.time()
+            self.process_gradients_exchange(j)
+            self.update_one_layer_parameters(j, self.TOTAL_BATCH)
+            ts2 = time.time()
+            self.Log('update {} layer cost time: {}'.format(j, (ts2 - ts1) * 1000.))
+
+
+        finsh_signal = pickle.dumps(np.zeros(1))
+        return communication_pb2.RawReply(signal=finsh_signal)
+    def Log(self, message):
+        print(message)
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10), options=[('grpc.max_message_length', 1024*1024*1024), ('grpc.max_send_message_length', 1024*1024*1024), ('grpc.max_receive_message_length', 1024*1024*1024)])
+    communication_pb2_grpc.add_CommServicer_to_server(Connecter(), server)
+    server.add_insecure_port("[::]:50052")
+    server.start()
+    try:
+        while True:
+            time.sleep(_ONE_DAY_IN_SECONDS)
+    except KeyboardInterrupt:
+        server.stop(0)
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    serve()
